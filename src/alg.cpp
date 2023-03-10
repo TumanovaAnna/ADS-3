6bf00308b93e535805484d61a59e63a6e090718c
TumanovaAnna <118038015+TumanovaAnna@users.noreply.github.com>
2023-03-10 09:58:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
-
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int minn = 0;
+    int maxx = size - 1;
+    int rsl = 0;
+    while (minn <= maxx) {
+        int midd = (maxx + minn) / 2;
+        if (value > arr[midd]) {
+            minn = midd + 1;
+        } else if (value < arr[midd]) {
+            maxx = midd - 1;
+        } else {
+            for (int i = 1; arr[midd] == arr[midd - i]; i++) rsl++;
+            for (int i = 0; arr[midd] == arr[midd + i]; i++) rsl++;
+            return rsl;
+        }
+    }
+    return 0;
 }
